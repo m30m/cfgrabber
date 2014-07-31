2d897220f8b35fa280a2c9a5616b62306bd629e8
Amin Moghaddam <amin.moghaddamV@gmail.com>
2014-08-01 00:20:31 +0430

    Now we can extract the submission table

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
+import sys
+import urllib
+import re
+
 __author__ = 'amin'
 
-import sys
 
+def geturl(url):
+  return urllib.urlopen(url).read()
+
+
+def get_submission_table(html):
+  """
+  Extracts the submission table from the complete html page
+  """
+  #Warning the RegExp must be fixed. we are using the fact that the submission table is the last table in the html doment
+  return re.search(r'<table class="status-frame-datatable">[\s\S]*</table>', html).group()
 
 
 def main():
