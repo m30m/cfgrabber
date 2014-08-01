077de458150cdc49c90459e024cf21d6e7c01376
Amin Moghaddam <amin.moghaddamV@gmail.com>
2014-08-01 13:53:44 +0430

    Changed have_next function to a simpler version  Completed get_source function  added gym handling

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 import sys
 import urllib
 import re
+import HTMLParser
 from xml.dom import minidom
 
 __author__ = 'amin'
 
 
+def get_url(url):
+  return urllib.urlopen(url).read()
+
+
 class Submission:
+  parser = HTMLParser.HTMLParser()
   def __init__(self, rowdom):
     """
     Given the Row DOM Extracting all the information about the submission
@@ -20,13 +26,23 @@ class Submission:
     self.time = cells[6]
     self.memory = cells[7]
     self.problem_link = name.attributes['href'].value
-    (self.contest_id, self.problem_id) = re.search(r'/problemset/problem/(\d+)/(\w+)', self.problem_link).groups()
+    self.isgym = self.problem_link.find('gym') != -1
+    (self.contest_id, self.problem_id) = re.search(r'/problemset/\w+/(\d+)/(\w+)', self.problem_link).groups()
+    self.contest_id = int(self.contest_id)
     self.name = name.childNodes[0].toxml()
     return
 
-
-def get_url(url):
-  return urllib.urlopen(url).read()
+  def get_source(self):
+    if self.isgym:
+      source_url = 'http://codeforces.com/gym/%d/submission/%d' % (self.contest_id, self.id)
+    else:
+      source_url = 'http://codeforces.com/contest/%d/submission/%d' % (self.contest_id, self.id)
+    html = get_url(source_url)
+    start_tag = '<pre class="prettyprint" style="padding:0.5em;">'
+    end_tag = '</pre>'
+    p1=html.find(start_tag)+len(start_tag)
+    p2=html.find(end_tag,p1)
+    return Submission.parser.unescape(html[p1:p2])
 
 
 def get_submission_table(html):
@@ -34,6 +50,7 @@ def get_submission_table(html):
   Extracts the submission table from the complete html page because the whole page XML is not well-formed
   """
   #Warning the RegExp must be fixed. we are using the fact that the submission table is the last table in the html document
+  #TODO: just remove the RegExp and do it with simple finding and stuff
   return re.search(r'<table class="status-frame-datatable">[\s\S]*</table>', html).group()
 
 
@@ -42,7 +59,7 @@ def get_submissions_url(handle, page):
 
 
 def have_next(html):
-  return not re.search(r'<span class="inactive">&rarr;</span>', html)
+  return html.find(r'<span class="inactive">&rarr;</span>')==-1
 
 
 def get_submissions(handle):
